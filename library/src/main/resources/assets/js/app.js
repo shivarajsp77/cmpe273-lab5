ab00c0b4d3a2f7a23ab3bd5b70f05e2bd2c55677
shivarajsp77 <shivarajsp77@gmail.com>
2014-04-12 23:48:54 -0700

    Final

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,43 @@
 $(":button").click(function() {
-	var isbn = this.id;
-	alert('About to report lost on ISBN ' + isbn);
+        var isbn = this.id;
+        alert('About to report lost on ISBN ' + isbn);
+        useIsbn(isbn);
+        $("button#"+isbn).attr("disabled","disabled");
+        window.location.reload(true);
 });
+
+$(document).ready(function(){
+    var items = $('td[title="status"]');
+    for( i=0; i<items.length; i++ ){
+            var status = items[i].innerHTML;
+            var isbn = items[i].id.slice("6");
+            var secId = "button#"+isbn;
+            if(status == "lost")
+                    {
+                            $(secId).attr("disabled",true);
+                    }
+            else{
+                    $(secId).removeAttr("disabled");
+            }
+    }
+    
+});
+
+function useIsbn(isbn)
+{
+        $.ajax({
+            type: "PUT",
+            url: "/library/v1/books/"+isbn+"?status=lost",
+            contentType: "application/json",
+            success: function(data) {
+            	$("#status_"+isbn).html("lost");
+                $("#"+isbn).attr("disabled","disabled");
+                
+            },
+            error: function() {
+           
+            }
+        });
+        $(status).text("lost")
+        
+}
